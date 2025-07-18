774885b6dfecaaba16d99bc0d33790ac62ae1b90
Sawadogo Steven <stevencheicksawadogo@gmail.com>
2025-07-18 14:55:25 +0000

    deuxieme commit

---
+++ src/assets/assets.js
@@ -0,0 +1,9 @@
+import logo from './logo2.png';
+// import header from './header.png';
+import logo_home from './logo-home.png';
+
+export const assets = {
+    logo,
+    // header,
+    logo_home,
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import MenuBar from "../components/MenuBar.jsx";
+
+const Home = () => {
+    return (
+        <MenuBar />
+    )
+}
+
+export default Home;
